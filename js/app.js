e83e9c5e4a041e5c2bd67911c4653fa51b05373d
Slava Shyharenko <shyharenkoslava@gmail.com>
2025-02-08 23:14:08 +0200

    push

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
-// Отримуємо елементи для взаємодії
-const catalogLink = document.getElementById("catalog-link");
-const modalCatalog = document.getElementById("modal-catalog");
+document.addEventListener("DOMContentLoaded", function () {
+  const catalogLink = document.getElementById("catalog-link");
+  const modalCatalog = document.getElementById("modal-catalog");
+  const modalBackdrop = document.querySelector(".modal-backdrop");
 
-// Відкриття модального вікна при натисканні на "Каталог"
-catalogLink.addEventListener("click", function (event) {
-  event.preventDefault(); // Зупиняємо перехід по посиланню
-  modalCatalog.classList.add("menu-is-open"); // Додаємо клас для відкриття
-});
+  // Відкриття модального вікна при натисканні на "Каталог"
+  catalogLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    modalCatalog.classList.add("menu-is-open");
+    modalCatalog.classList.remove("menu-is-closing");
+  });
 
-// Закриття модального вікна при натисканні на фон
-modalCatalog.addEventListener("click", function (event) {
-  if (event.target === modalCatalog) {
-    // Перевіряємо, чи натиснуто саме на фон
-    modalCatalog.classList.remove("menu-is-open"); // Закриваємо модальне вікно
-  }
+  // Закриття модального вікна при натисканні будь-де поза модальним вікном
+  document.addEventListener("click", function (event) {
+    if (
+      !modalBackdrop.contains(event.target) &&
+      !catalogLink.contains(event.target)
+    ) {
+      modalCatalog.classList.add("menu-is-closing");
+      setTimeout(() => {
+        modalCatalog.classList.remove("menu-is-open", "menu-is-closing");
+      }, 350);
+    }
+  });
 });
