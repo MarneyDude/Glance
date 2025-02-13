09da2ec9033d987b6136b2d2674562f853cfd9c0
Slava Shyharenko <shyharenkoslava@gmail.com>
2025-02-13 23:29:12 +0200

    fixed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const link = document.querySelector("#catalog-link");
-const modalCatalog = document.querySelector(".modal-catalog");
+const modalCatalog = document.querySelector(".modal-overlay");
 const homeLink = document.querySelector("#home-link");
+const catalogLinkEl = document.querySelectorAll(".catalog-link");
+const catalogNameEl = document.querySelector(".filter-title");
 
 link.addEventListener("click", (event) => {
   event.preventDefault();
-
+  modalCatalog.scrollTop = 0;
   if (modalCatalog.classList.contains("menu-is-open")) {
     document.body.style.overflow = "auto";
   } else {
@@ -20,4 +22,24 @@ homeLink.addEventListener("click", (event) => {
   document.body.style.overflow = "auto";
 
   modalCatalog.classList.remove("menu-is-open");
+});
+
+let catalogName = "";
+
+catalogLinkEl.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    catalogName = event.target.closest("a").textContent.trim();
+
+    localStorage.setItem("selectedCatalog", catalogName);
+
+    catalogNameEl.textContent = catalogName;
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const catalogName = localStorage.getItem("selectedCatalog");
+
+  if (catalogName) {
+    catalogNameEl.textContent = catalogName;
+  }
 });
