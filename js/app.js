a381cccf75e4edcea9b9ce0642d4db28a1948471
Slava Shyharenko <shyharenkoslava@gmail.com>
2025-02-21 14:44:22 +0200

    add shares list js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,257 @@
-const catalogLinkEl = document.querySelector("#catalog-link");
-const homeLinkEl = document.querySelector("#home-link");
-const modalCatalogEl = document.querySelector(".modal__overlay");
-const catalogLinksEl = document.querySelectorAll(".catalog-link");
-console.log("catalogLinksEl:", catalogLinksEl);
-const catalogNameEl = document.querySelector(".filter-title");
-const currentPathEl = window.location.pathname;
+const catalog = {
+  phones: [
+    {
+      brand: "Apple",
+      model: "iPhone 14",
+      price: 999,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 50,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/uaua_iphone14_midnight_pdp_image_position-1a_2.jpg",
+    },
+    {
+      brand: "Samsung",
+      model: "Galaxy S23",
+      price: 899,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 30,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/s/m/sm-s721_galaxys24fe_graphite_front.jpg",
+    },
+    {
+      brand: "Xiaomi",
+      model: "Redmi Note 13 Pro +",
+      price: 499,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 100,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/_/8/_8_45_.jpg",
+    },
+    {
+      brand: "OnePlus",
+      model: "9 Pro",
+      price: 799,
+      discountPercentage: 6, // Знижка 6%
+      quantity: 80,
+      colors: ["Black", "Silver", "Green"],
+      features: ["5G", "Fluid AMOLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/6/0/60ee98bb73879_9pro-morning-mist-02-rgb_1_.jpg",
+    },
+    {
+      brand: "Poco",
+      model: "X7 Pro 8",
+      price: 599,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 60,
+      colors: ["White", "Black", "Green"],
+      features: ["5G", "OLED Display", "Google Assistant"],
+      photo:
+        "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/_/8/_8.29_.jpg/w_320",
+    },
+  ],
+  smartWatches: [
+    {
+      brand: "Apple",
+      model: "Watch 5",
+      price: 399,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 40,
+      colors: ["Gold", "Silver", "Black"],
+      features: ["Heart Rate Monitor", "GPS", "Waterproof"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/a/p/apple_watch_se_44mm_gps_midnight_aluminum_sport_band_midnight_pdp_image_position_1__ce-ww_result.webp",
+    },
+    {
+      brand: "Samsung",
+      model: "Galaxy Watch 7",
+      price: 299,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 60,
+      colors: ["Gold", "Silver", "Black"],
+      features: ["Heart Rate Monitor", "GPS", "Waterproof"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/s/m/sm-l305_002_front2_cream_240429_result.webp",
+    },
+    {
+      brand: "Garmin",
+      model: "Fenix 6",
+      price: 599,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 45,
+      colors: ["Black", "Silver", "Blue"],
+      features: ["GPS", "Heart Rate Monitor", "Solar Charging"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/3664734313069868.jpg",
+    },
+    {
+      brand: "Amazfit",
+      model: "Pop 3S",
+      price: 179,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 75,
+      colors: ["Black", "Silver", "Gold"],
+      features: ["Heart Rate Monitor", "GPS", "Fitness Tracking"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/1/_/1_result1689945534.jpg",
+    },
+    {
+      brand: "Fitbit",
+      model: "Charge 4",
+      price: 129,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 90,
+      colors: ["Black", "Plum", "White"],
+      features: ["Heart Rate Monitor", "Sleep Tracking", "GPS"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/2653566511083473.jpg",
+    },
+  ],
+  tV: [
+    {
+      brand: "LG",
+      model: "OLED C2",
+      price: 1299,
+      discountPercentage: 7, // Знижка 7%
+      quantity: 15,
+      colors: ["Black", "Silver", "Grey"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/5/5/55_c4_a_1.jpg",
+    },
+    {
+      brand: "Samsung",
+      model: "QLED Q90",
+      price: 1099,
+      discountPercentage: 9, // Знижка 9%
+      quantity: 20,
+      colors: ["Black", "Silver", "Grey"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/q/e/qe65s95cauxua.jpg",
+    },
+    {
+      brand: "Sony",
+      model: "Bravia XR",
+      price: 1499,
+      discountPercentage: 7, // Знижка 7%
+      quantity: 10,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/a/s/assets_asset_hierarchy_consumer_assets_television_bravia_lcd_hdtv_fy_23_a80l_ecomm_product_images_xr55-65-77a80l_01-a80l-55-65-77-sony-bravia_xr-frnt_1.jpg",
+    },
+    {
+      brand: "Philips",
+      model: "V-Series",
+      price: 499,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 40,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/p/h/philips_55oled769_amb_20_1_1_.jpg",
+    },
+    {
+      brand: "TCL",
+      model: "6-Series",
+      price: 899,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 25,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Roku TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/0/_/0_tcl-805-55.jpg",
+    },
+  ],
+};
 
-catalogLinkEl.addEventListener("click", handleCatalogClick);
-homeLinkEl.addEventListener("click", closeCatalog);
-catalogLinksEl.forEach((link) =>
-  link.addEventListener("click", handleCatalogSelection)
-);
+const menuLinksEl = document.querySelector("#js-menu-list");
+const modalCatalogEl = document.querySelector(".js-modal-catalog");
+const sharesListEl = document.querySelector("#js-shares-list");
 
-function openModal() {
-  modalCatalogEl.scrollTop = 0;
-  modalCatalogEl.classList.toggle("menu__is-open");
-  document.body.style.overflow = modalCatalogEl.classList.contains(
-    "menu__is-open"
-  )
-    ? "hidden"
-    : "auto";
-}
+menuLinksEl.addEventListener("click", openLink);
+renderingShares();
 
-function closeCatalog() {
-  modalCatalogEl.classList.remove("menu__is-open");
-  document.body.style.overflow = "auto";
-}
-
-function handleCatalogClick(event) {
+function openLink(event) {
   event.preventDefault();
-  openModal();
+  const homeLink = event.target.classList.contains("menu__link--home");
+  const catalogLink = event.target.classList.contains("menu__link--catalog");
+  const basketLink = event.target.classList.contains("menu__link--basket");
+  const profileLink = event.target.classList.contains("menu__link--profile");
+
+  if (homeLink) {
+    if (modalCatalogEl.classList.contains("menu__is-open")) {
+      modalCatalogEl.classList.remove("menu__is-open");
+    }
+  } else if (catalogLink) {
+    modalCatalogEl.classList.toggle("menu__is-open");
+  } else if (basketLink) {
+    console.log("basketPage");
+  } else if (profileLink) {
+    console.log("profilePage");
+  }
 }
 
-function handleCatalogSelection(event) {
-  const selectedLinkEl = event.target.closest("a");
-  if (!selectedLinkEl) return;
+function renderingShares() {
+  const markup = catalog.phones
+    .map((phone) => {
+      const {
+        brand,
+        model,
+        price,
+        discountPercentage,
+        quantity,
+        colors,
+        features,
+        photo,
+      } = phone;
 
-  const catalogName = selectedLinkEl.textContent.trim();
-  localStorage.setItem("selectedCatalog", catalogName);
-  catalogNameEl.textContent = catalogName;
+      const discountPrice = calculateDiscount(price, discountPercentage);
+
+      return `<li class="shares__item">
+                 <div class="shares-item-header">
+              <img class="shares__image" src="${photo}" alt="phone" />
+              <ul class="shares__color-list">
+                <li class="shares__color-item">
+                  <a href="#" class="shares__color-link"></a>
+                </li>
+                <li class="shares__color-item">
+                  <a href="#" class="shares__color-link"></a>
+                </li>
+                <li class="shares__color-item">
+                  <a href="#" class="shares__color-link"></a>
+                </li>
+              </ul>
+            </div>
+              <h3 class="shares__name">${brand + " " + model}</h3>
+              <div class="shares__price-box">
+                <p class="shares__price shares__price--discount">${discountPrice} $</p>
+                <p class="shares__price shares__price--original">${price} $</p>
+              </div>
+              <div class="shares__info">
+                <p class="shares__stock">In stock</p>
+                <svg class="shares__like-icon" width="21" height="17px">
+                  <use href="./images/sprite.svg#heart"></use>
+                </svg>
+              </div>
+              <button class="shares__button" type="button">To Cart</button>
+            </li>`;
+    })
+    .join("");
+
+  sharesListEl.insertAdjacentHTML("afterbegin", markup);
+}
+
+function calculateDiscount(price, discountPercentage) {
+  const discountAmount = (price * discountPercentage) / 100;
+  const discountedPrice = price - discountAmount;
+
+  return Math.round(discountedPrice);
 }
--- js/catalog.js
+++ js/catalog.js
@@ -1,83 +1,173 @@
 const catalog = {
-  phones: {
-    brand: ["Apple", "Samsung", "Xiaomi"],
-    model: ["iPhone 14", "Galaxy S23", "Redmi Note 12"],
-    price: [999, 899, 499],
-    features: ["5G", "OLED Display", "Fast Charging"],
-    photo: ["iphone14.png", "galaxyS23.png", "redmiNote12.png"],
-  },
-  smartWatches: {
-    brand: ["Apple", "Samsung", "Garmin"],
-    model: ["Apple Watch 8", "Galaxy Watch 5", "Garmin Fenix 7"],
-    price: [399, 299, 599],
-    features: ["Heart Rate Monitor", "GPS", "Waterproof"],
-    photo: ["appleWatch8.png", "galaxyWatch5.png", "garminFenix7.png"],
-  },
-  tV: {
-    brand: ["LG", "Samsung", "Sony"],
-    model: ["OLED C2", "QLED Q90", "Bravia XR"],
-    price: [1299, 1099, 1499],
-    features: ["4K", "HDR", "Smart TV"],
-    photo: ["lgC2.png", "samsungQ90.png", "sonyBraviaXR.png"],
-  },
-  audio: {
-    brand: ["Bose", "Sony", "JBL"],
-    model: ["QuietComfort 45", "WH-1000XM5", "Flip 6"],
-    price: [329, 399, 149],
-    features: ["Noise Cancelling", "Wireless", "Bass Boost"],
-    photo: ["boseQC45.png", "sonyWH1000XM5.png", "jblFlip6.png"],
-  },
-  stylingTools: {
-    brand: ["Dyson", "Philips", "Remington"],
-    model: ["Airwrap", "Prestige", "Pearl Pro"],
-    price: [549, 249, 79],
-    features: ["Multiple Attachments", "Ionic Technology", "Fast Drying"],
-    photo: ["dysonAirwrap.png", "philipsPrestige.png", "remingtonPearlPro.png"],
-  },
-  gamingConsoles: {
-    brand: ["Sony", "Microsoft", "Nintendo"],
-    model: ["PlayStation 5", "Xbox Series X", "Switch OLED"],
-    price: [499, 499, 349],
-    features: ["4K Gaming", "Ray Tracing", "Exclusive Titles"],
-    photo: ["ps5.png", "xboxSeriesX.png", "switchOLED.png"],
-  },
-  stoves: {
-    brand: ["Bosch", "Samsung", "LG"],
-    model: ["Serie 8", "Flex Duo", "ThinQ"],
-    price: [1099, 999, 899],
-    features: ["Induction", "Smart Cooking", "Self-Cleaning"],
-    photo: ["boschSerie8.png", "samsungFlexDuo.png", "lgThinQ.png"],
-  },
-  tourism: {
-    brand: ["Deuter", "North Face", "Osprey"],
-    model: ["Futura 32", "Base Camp", "Atmos AG"],
-    price: [159, 199, 279],
-    features: ["Waterproof", "Lightweight", "Ergonomic Design"],
-    photo: ["deuterFutura32.png", "northFaceBaseCamp.png", "ospreyAtmosAG.png"],
-  },
-  homeLiving: {
-    brand: ["IKEA", "Philips", "Dyson"],
-    model: ["Kallax", "Hue", "V11"],
-    price: [99, 199, 599],
-    features: ["Modular", "Smart Lighting", "Cordless"],
-    photo: ["ikeaKallax.png", "philipsHue.png", "dysonV11.png"],
-  },
-  forChildren: {
-    brand: ["LEGO", "Fisher-Price", "Chicco"],
-    model: ["Classic Set", "Laugh & Learn", "Next2Me"],
-    price: [49, 39, 199],
-    features: ["Educational", "Safe Materials", "Adjustable"],
-    photo: [
-      "legoClassic.png",
-      "fisherPriceLaughLearn.png",
-      "chiccoNext2Me.png",
-    ],
-  },
-  perfumes: {
-    brand: ["Dior", "Chanel", "Armani"],
-    model: ["Sauvage", "Coco Mademoiselle", "Acqua di Gio"],
-    price: [89, 119, 99],
-    features: ["Long Lasting", "Elegant Scent", "Luxury Packaging"],
-    photo: ["diorSauvage.png", "chanelCoco.png", "armaniAcquaDiGio.png"],
-  },
+  phones: [
+    {
+      brand: "Apple",
+      model: "iPhone 14",
+      price: 999,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 50,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/uaua_iphone14_midnight_pdp_image_position-1a_2.jpg",
+    },
+    {
+      brand: "Samsung",
+      model: "Galaxy S23",
+      price: 899,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 30,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/s/m/sm-s721_galaxys24fe_graphite_front.jpg",
+    },
+    {
+      brand: "Xiaomi",
+      model: "Redmi Note 13 Pro +",
+      price: 499,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 100,
+      colors: ["Silver", "Black", "Blue"],
+      features: ["5G", "OLED Display", "Fast Charging"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/1/1/11111_7.jpg ",
+    },
+    {
+      brand: "OnePlus",
+      model: "OnePlus 11",
+      price: 799,
+      discountPercentage: 6, // Знижка 6%
+      quantity: 80,
+      colors: ["Black", "Silver", "Green"],
+      features: ["5G", "Fluid AMOLED Display", "Fast Charging"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/4978120413352997.webp",
+    },
+    {
+      brand: "Google",
+      model: "Pixel 7",
+      price: 599,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 60,
+      colors: ["White", "Black", "Green"],
+      features: ["5G", "OLED Display", "Google Assistant"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/5033195512300116.webp",
+    },
+  ],
+  smartWatches: [
+    {
+      brand: "Apple",
+      model: "Apple Watch 5",
+      price: 399,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 40,
+      colors: ["Gold", "Silver", "Black"],
+      features: ["Heart Rate Monitor", "GPS", "Waterproof"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/a/p/apple_watch_se_44mm_gps_midnight_aluminum_sport_band_midnight_pdp_image_position_1__ce-ww_result.webp",
+    },
+    {
+      brand: "Samsung",
+      model: "Galaxy Watch 7",
+      price: 299,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 60,
+      colors: ["Gold", "Silver", "Black"],
+      features: ["Heart Rate Monitor", "GPS", "Waterproof"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/s/m/sm-l305_002_front2_cream_240429_result.webp",
+    },
+    {
+      brand: "Garmin",
+      model: "Fenix 6",
+      price: 599,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 45,
+      colors: ["Black", "Silver", "Blue"],
+      features: ["GPS", "Heart Rate Monitor", "Solar Charging"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/3664734313069868.jpg",
+    },
+    {
+      brand: "Amazfit",
+      model: "Pop 3S",
+      price: 179,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 75,
+      colors: ["Black", "Silver", "Gold"],
+      features: ["Heart Rate Monitor", "GPS", "Fitness Tracking"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/1/_/1_result1689945534.jpg",
+    },
+    {
+      brand: "Fitbit",
+      model: "Charge 4",
+      price: 129,
+      discountPercentage: 8, // Знижка 8%
+      quantity: 90,
+      colors: ["Black", "Plum", "White"],
+      features: ["Heart Rate Monitor", "Sleep Tracking", "GPS"],
+      photo:
+        "https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/import/2653566511083473.jpg",
+    },
+  ],
+  tV: [
+    {
+      brand: "LG",
+      model: "OLED C2",
+      price: 1299,
+      discountPercentage: 7, // Знижка 7%
+      quantity: 15,
+      colors: ["Black", "Silver", "Grey"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/5/5/55_c4_a_1.jpg",
+    },
+    {
+      brand: "Samsung",
+      model: "QLED Q90",
+      price: 1099,
+      discountPercentage: 9, // Знижка 9%
+      quantity: 20,
+      colors: ["Black", "Silver", "Grey"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/q/e/qe65s95cauxua.jpg",
+    },
+    {
+      brand: "Sony",
+      model: "Bravia XR",
+      price: 1499,
+      discountPercentage: 7, // Знижка 7%
+      quantity: 10,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/a/s/assets_asset_hierarchy_consumer_assets_television_bravia_lcd_hdtv_fy_23_a80l_ecomm_product_images_xr55-65-77a80l_01-a80l-55-65-77-sony-bravia_xr-frnt_1.jpg",
+    },
+    {
+      brand: "Philips",
+      model: "V-Series",
+      price: 499,
+      discountPercentage: 10, // Знижка 10%
+      quantity: 40,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Smart TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/p/h/philips_55oled769_amb_20_1_1_.jpg",
+    },
+    {
+      brand: "TCL",
+      model: "6-Series",
+      price: 899,
+      discountPercentage: 11, // Знижка 11%
+      quantity: 25,
+      colors: ["Black", "Silver"],
+      features: ["4K", "HDR", "Roku TV"],
+      photo:
+        "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/0/_/0_tcl-805-55.jpg",
+    },
+  ],
 };
